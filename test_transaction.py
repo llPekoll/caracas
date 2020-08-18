b9263be2606c1a05e727946dc457d353eb59cc45
peko <yoyo.mepa@gmail.com>
2020-08-18 15:13:45 +0200

    fix test :cold_sweat:

--- test_transaction.py
+++ test_transaction.py
@@ -1,4 +1,15 @@
-from main import app
+from main import app as flask_app
+import pytest
+
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
 
 def test_index(app, client):
     res = client.get('/')
